155a9d36f73e6b3a395b4619c6a8e566468ea119
Anna Karlsen <146820987+annkar73@users.noreply.github.com>
2025-09-16 19:40:52 +0200

    update contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,15 +2,19 @@ export default function ContactPage() {
   return (
     <div>
       <h1 className="text-3xl pt-8 font-bold mb-4">Kontakt</h1>
-      <p>Öppettider och kontaktinformation kommer här.</p>
+      <p>Observera avvikande öppettider under vinterhalvåret. 2025 innebär det att öppettiderna ändras från 27 oktober.</p>
 
             {/* Contact info */}
-      <div className="mt-6 pt-6 border-t border-gray-300 pt-1 text-left text-sm print:text-[10px]">
-        <p className="font-bold text-lg">Pizzahuset i Hallstavik</p>
-        <p><span className="font-semibold font-green-800">Adress:</span> Uppsalavägen 4C, Hallstavik</p>
-        <p><span className="font-semibold">Öppettider:</span> Mån-Sön 11:00-21:00 | vintertid: 11:00-20:00</p> 
+      <div className="mt-6 pt-6 border-t border-gray-300 pt-1 text-left text-base md:text-lg print:text-[10px]">
+        <p className="font-bold text-2xl md:text-3xl text-red-800 pb-4">Pizzahuset i Hallstavik</p>
+        <p><span className="font-semibold text-green-800">Adress:</span> Uppsalavägen 4C, Hallstavik</p>
+        <p><span className="font-semibold text-green-800">Öppettider:</span> Mån-Sön 11:00-21:00 | vintertid: 11:00-20:00</p> 
 
-        <p><span className="font-semibold">Telefon:</span> 0175-234 51</p>
+        <p><span className="font-semibold text-green-800">Telefon:</span> 0175-234 51</p>
+      </div>
+
+      <div className="mt-6 pt-6 border-t border-gray-300 pt-1 text-left text-sm print:text-[10px]">
+        här ska jag lägga en kartbild
       </div>
 
 
